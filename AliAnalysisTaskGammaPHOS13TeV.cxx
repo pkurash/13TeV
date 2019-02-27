01e8b5ba965512fc08dba3966212c10a16aa7f77
Podist Kurashvili <pkurash@cern.ch>
2019-02-27 18:25:44 +0100

    Analysis code

--- AliAnalysisTaskGammaPHOS13TeV.cxx
+++ AliAnalysisTaskGammaPHOS13TeV.cxx
@@ -1,21 +1,4 @@
-/**************************************************************************
- * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
- *                                                                        *
- * Author: The ALICE Off-line Project.                                    *
- * Contributors are mentioned in the code where appropriate.              *
- *                                                                        *
- * Permission to use, copy, modify and distribute this software and its   *
- * documentation strictly for non-commercial purposes is hereby granted   *
- * without fee, provided that the above copyright notice appears in all   *
- * copies and that both the copyright notice and this permission notice   *
- * appear in the supporting documentation. The authors make no claims     *
- * about the suitability of this software for any purpose. It is          *
- * provided "as is" without express or implied warranty.                  *
- **************************************************************************/
-/* $Id: AliAnalysisTaskGammaPHOS13TeV.cxx 56291 2012-05-08 15:58:57Z kharlov $ */
- 
-// Analysis task gamma analysis in pp collisions
-
+// Analysis task gamma analysis in pp collision
 #include <TSystem.h>
 #include <TFile.h>
 #include <TKey.h>
